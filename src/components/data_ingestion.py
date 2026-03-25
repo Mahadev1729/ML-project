2a0c790d38f93eb1b2aafa7c6ba9e54558f13577
Mahadev1729 <devcampus17@gmail.com>
2026-03-25 21:55:55 +0530

    done with data ingestion compenent

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,6 +25,15 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train test split inited")
-        except:
-            pass    
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of data is failed")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+                
+            )
+        except Exception as e: 
+            raise CustomException(e.sys)
                      
