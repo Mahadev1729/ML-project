049af4fe2551ee2a8acb105e53f59d8a79dbae00
Mahadev1729 <devcampus17@gmail.com>
2026-03-25 22:39:26 +0530

    try to implement the data transformation feature

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,35 @@
-﻿
+﻿import sys
+from dataclasses import dataclass
+import numpy as np
+import os
+import pandas as pd 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifact',"preprocessor.pkl")
+    
+class DataTransformation:
+        def __init__(self):
+            self.Data_Transformation_Config=DataTransformationConfig()
+        def get_data_transformer_object(self):
+            try:
+                numerical_features=['writing score','reading score']
+                cat_features=['gender',
+                    'race/ethnicity',
+                    'parental level of education',
+                    'lunch',
+                    'test preparation course'
+                    ]
+            except:
+                pass
+            
+                
+            
+    
+
