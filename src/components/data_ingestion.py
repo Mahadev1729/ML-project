748713a87890a27f98428d69968173db7755a72f
Mahadev1729 <devcampus17@gmail.com>
2026-03-26 16:12:08 +0530

    impment feature of model pickling and saving on local disk

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
@@ -6,6 +6,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
 
 @dataclass
@@ -20,7 +21,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\Stud.csv')
+            df = pd.read_csv(r'notebook\data\Stud.csv')
             logging.info('read the dataset as dataframe')
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
@@ -39,4 +40,9 @@ class DataIngestion:
                      
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,4 @@
-﻿import sys
+import sys
 from dataclasses import dataclass
 import numpy as np
 import os
@@ -11,6 +11,7 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 
+from src.utilis import save_object
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifact',"preprocessor.pkl")
     
@@ -41,7 +42,7 @@ class DataTransformation:
                     steps=[
                         ("imputer",SimpleImputer(strategy="most_frequent")),
                         ("one_hot_encoder",OneHotEncoder()),
-                        ("scaling",StandardScaler())
+                        ("scaling",StandardScaler(with_mean=False))
                     ]
                 )
                 logging.info(f"Categerical columns:{cat_features}")
@@ -55,8 +56,51 @@ class DataTransformation:
                 return preprocessor
             except Exception as e:
                 raise CustomException(e,sys)
-            
+        def initiate_data_transformation(self, train_path,test_path):
+            try:
+                train_df=pd.read_csv(train_path)
+                test_df=pd.read_csv(test_path)
                 
-            
+                logging.info("read and train test data completed")
+                
+                logging.info("Obtain preprocessing object")
+                preprocessor_obj=self.get_data_transformer_object()
+                target_column_name="math score"
+                numerical_features=['writing score','reading score']
+                
+                input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+                target_feature_train_df=train_df[target_column_name]
+                
+                input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+                target_feature_test_df=test_df[target_column_name]
+                
+                logging.info(
+                    f"Applying preprocessing object on training dataframe  and testing dataFrame"
+                )
+                input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+                input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+                
+                train_arr=np.c_[
+                    input_feature_train_arr,np.array(target_feature_train_df)
+                    
+                ]
+                test_arr=np.c_[
+                    input_feature_test_arr,np.array(target_feature_test_df)
+                ]
+                
+                logging.info(f"saved preprocessig object.")
+                save_object(
+                    
+                    file_path=self.Data_Transformation_Config.preprocessor_obj_file_path,
+                    obj=preprocessor_obj
+                )
+                
+                return (
+                    train_arr,
+                    test_arr,
+                    self.Data_Transformation_Config.preprocessor_obj_file_path
+                )
+            except Exception as e:
+                raise CustomException(e,sys)
     
 
